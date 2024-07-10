f1ac8e0d722ddb6d7c386a7b075e762b3ba62515
Dave <davewalker777@yandex.ru>
2024-07-10 16:37:15 +0300

    pytest

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+# myapp/tests/test_models.py
+import pytest
+from django.utils import timezone
+from main.models import Theme, Task
 
-# Create your tests here.
+@pytest.fixture
+def theme():
+    return Theme.objects.create(title="Учеба")
+
+@pytest.fixture
+def task(theme):
+    return Task.objects.create(
+        theme=theme,
+        title="Математика",
+        task="Решить примеры",
+        date=timezone.now().date(),
+        time=timezone.now().time()
+    )
+
+@pytest.mark.django_db
+def test_theme_creation(theme):
+    assert theme.title == "Учеба"
+    assert str(theme) == "Учеба"
+
+@pytest.mark.django_db
+def test_task_creation(task):
+    assert task.title == "Математика"
+    assert task.task == "Решить примеры"
+    assert str(task) == "Математика"
+    assert task.theme.title == "Учеба"
+
+@pytest.mark.django_db
+def test_task_link_to_theme(theme, task):
+    assert task in theme.tasks.all()
